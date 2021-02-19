efb1a47aabc360a3563a7592c213fd153448ac36
Melissa Haskell <mhask@umich.edu>
2021-02-19 17:18:16 -0500

    Calls ir_mex_fun with variable number of args

--- mri/ir_mex_build_mri.m
+++ mri/ir_mex_build_mri.m
@@ -1,11 +1,6 @@
 % ir_mex_build_mri
 % run matlab's "mex" command to "compile" the mri-related code
 % into mex files.
-% only users on unsupported systems, e.g., PCs, will need to do this
-
-% I never use this file because I use "make" with ./Makefile instead.
-% This is just an example.
-% You might have to modify the flags for your system.
 
 dir_current = pwd;
 dir_mri = path_find_dir('mri');
