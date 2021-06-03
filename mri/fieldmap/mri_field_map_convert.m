702cd71cbdfe64726a5c1dba9f610afa8a154329
Melissa Haskell <mhask@umich.edu>
2021-06-03 18:17:47 -0400

    updated method for calling Lin fmap functions using wrapper

--- mri/fieldmap/mri_field_map_convert.m
+++ mri/fieldmap/mri_field_map_convert.m
@@ -1,42 +1,33 @@
-function [wmap, wconv] = fmap_est_qm_wrapper(yik, etime, varargin)
+function [wmap, wconv] = mri_field_map_convert(yik, etime, func_name, varargin)
 % This function is a wrapper for fmap_est_qm.m to make it callable in a
 % similar fashion as mri_field_map_reg.m
 
+%| inputs:
 %|	yik	[(N) nset ncoil]	complex images at different echo times
 %|				(these should be undistorted)
 %|				(N) can be (nx,ny) or (nx,ny,nz) etc.
 %|	etime	[nset]		vector of echo times [usually in seconds]
+%|  func_name   string specifying which function to call
 %|
 %| options
-%|	l2b		log_2(beta), regularization parameter (default: -3)
-%|	order		regularization order (default: 2)
-%|	niter		# of iterations (default: 40)
-%|	fmax		percent of median used in calculating scale factor (.1)
-%| 	wthresh		percent of median used in masking out for RMSerror (.1)
-%|	winit	[(N)]		initial field map for iterating
-%|				(default: estimate from first two scans)
-%|	mask	[(N)]		logical support mask (only estimate within this)
-%|  sens    [(N) ncoil] sensitivity maps
-%|  quiet       supresses all output from function
+%|	maskR	[(N)]		 logical support mask 
+%|  sens    [(N) ncoil]   sensitivity maps
+%|  quiet   supresses all output from function
+%|  winit   initial image
+%|
+%|	the rest of the options passed depend on which function is being 
+%|  called...
 %|
 %| out
 %|	wmap	[(N)]	regularized estimate of field map [rad/sec]
 %|	wconv	""	conventional estimate from first two scans
 
-% default optional arguments set here! If there are changes in
-% fmap_est_qm.m defaults they need to be changed here as well
-arg.niter = 30;
-arg.maskR = [];
-arg.order = 2;
-arg.l2b = -6;
-arg.hess = 'diag';
-arg.dim = 3; % diag curvature for 2d / 3d
-arg.df = 0;
-arg.relamp = 1;
+% default optional arguments
 arg.quiet = false;
 arg.winit = [];
 arg.sens = [];
-arg = vararg_pair(arg, varargin);
+arg.maskR = [];
+arg = vararg_pair(arg, varargin, 'allow_new', 1);
 
 
 % determine data size & reshape as needed
@@ -78,8 +69,6 @@ wconv = angle(stackpick(yik,2) .* conj(stackpick(yik,1))) ...
 if isempty(arg.winit), arg.winit = wconv; end
 w = arg.winit(:);
 
-%%%%%  call fmap_est_qm
-func_name = 'fmap_est_qm';
 
 % prep optional arguments to be passed into fmap function
 arg_fmap = arg;
---
+++ mri/fieldmap/mri_field_map_qm.m
@@ -0,0 +1,29 @@
+function [wmap, wconv] = mri_field_map_qm(yik, etime, varargin)
+% This function is a wrapper for fmap_est_qm.m to make it callable in a
+% similar fashion as mri_field_map_reg.m
+
+%|	yik	[(N) nset ncoil]	complex images at different echo times
+%|				(these should be undistorted)
+%|				(N) can be (nx,ny) or (nx,ny,nz) etc.
+%|	etime	[nset]		vector of echo times [usually in seconds]
+%|
+%| options
+%|	see fmap_est_qm.m for options
+%|
+%| out
+%|	wmap	[(N)]	regularized estimate of field map [rad/sec]
+%|	wconv	""	conventional estimate from first two scans
+
+
+arg = [];
+arg = vararg_pair(arg, varargin, 'allow_new', 1);
+
+%% call function
+varargin2 = arg2varargin(arg);
+func_name = 'fmap_est_qm';
+[wmap, wconv] = mri_field_map_convert(yik, etime, func_name, varargin2{:});
+
+
+
+end
+
