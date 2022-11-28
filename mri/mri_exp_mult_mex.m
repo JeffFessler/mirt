05bc60e0dd481c09e37acef0eda986a2e3d5b67a
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-11-28 10:06:59 -0500

    Do not run Matlab in mri/ folder

--- mri/mri_exp_mult_mex.m
+++ mri/mri_exp_mult_mex.m
@@ -1,2 +1,8 @@
 function mri_exp_mult_mex
-error 'if you get this message, then you need to compile mri_exp_mult_mex'
+tmp = which('mri_exp_mult_mex');
+[mridir, ~] = fileparts(tmp);
+if streq(pwd, mridir)
+    fail('Do not run Matlab from within the mri/ directory!')
+    % because then the path will put mri_exp_mult_mex.m before the mex file!
+end
+fail('You need to compile mri_exp_mult_mex.%s', mexext)
