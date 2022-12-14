61cd35452fa014dd90bdf928c92246a408bbc792
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-12-14 17:00:55 -0500

    Update extrema(f,x)

--- utilities/extrema.m
+++ utilities/extrema.m
@@ -1,5 +1,8 @@
 %| extrema(x)
 %| akin to julia extrema
-function out = extrema(x)
-   out = minmax(x);
+function out = extrema(f, x)
+if isa(f, 'function_handle')
+   out = minmax(f(x));
+else
+   out = minmax(f);
 end
